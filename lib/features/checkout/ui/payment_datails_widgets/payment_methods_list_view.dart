458228d72bed8092143219f0d007efb173b88203
tasneem-gamal <tasneemgamal044@gmail.com>
2024-11-06 18:57:55 -0800

    make method selected

--- lib/features/checkout/ui/payment_datails_widgets/payment_methods_list_view.dart
+++ lib/features/checkout/ui/payment_datails_widgets/payment_methods_list_view.dart
@@ -2,13 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:payment/features/checkout/ui/payment_datails_widgets/payment_method_item.dart';
 
-class PaymentMethodsListView extends StatelessWidget {
+class PaymentMethodsListView extends StatefulWidget {
   const PaymentMethodsListView({super.key});
-  
+
+  @override
+  State<PaymentMethodsListView> createState() => _PaymentMethodsListViewState();
+}
+
+class _PaymentMethodsListViewState extends State<PaymentMethodsListView> {
   final List<String> paymentMethodsImage = const [
     'assets/credit-card.png',
     'assets/paypal.png'
   ];
+  
+  int activeIndex = 0;
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -17,8 +24,15 @@ class PaymentMethodsListView extends StatelessWidget {
         separatorBuilder: (context, index) => const SizedBox(width: 12,),
         scrollDirection: Axis.horizontal,
         itemCount: paymentMethodsImage.length,
-        itemBuilder: (context, index) => PaymentMethodItem(
-          image: paymentMethodsImage[index]
+        itemBuilder: (context, index) => GestureDetector(
+          onTap: (){
+            activeIndex = index;
+            setState(() {});
+          },
+          child: PaymentMethodItem(
+            image: paymentMethodsImage[index],
+            isActive: activeIndex == index,
+          ),
         )
       ),
     );
