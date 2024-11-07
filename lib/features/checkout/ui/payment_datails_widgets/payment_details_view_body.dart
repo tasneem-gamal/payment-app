60f6f7cb6addb4063635409183f798412c22da53
tasneem-gamal <tasneemgamal044@gmail.com>
2024-11-06 18:53:51 -0800

    create payment method list view

--- lib/features/checkout/ui/payment_datails_widgets/payment_details_view_body.dart
+++ lib/features/checkout/ui/payment_datails_widgets/payment_details_view_body.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:payment/core/widgets/constants.dart';
 import 'package:payment/core/widgets/custom_app_bar.dart';
-import 'package:payment/features/checkout/ui/payment_datails_widgets/payment_method_item.dart';
+import 'package:payment/features/checkout/ui/payment_datails_widgets/payment_methods_list_view.dart';
 
 class PaymentDetailsViewBody extends StatelessWidget {
   const PaymentDetailsViewBody({super.key});
@@ -16,7 +16,7 @@ class PaymentDetailsViewBody extends StatelessWidget {
         children: [
           const CustomAppBar(text: 'Payment Details'),
           SizedBox(height: 24.h,),
-          PaymentMethodItem(isActive: false,image: 'assets/credit-card.png',)
+          PaymentMethodsListView()
         ],
       ),
     );
