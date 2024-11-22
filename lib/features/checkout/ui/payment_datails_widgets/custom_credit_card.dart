a7036ee69f9af06471751768d1de59985f0cd953
tasneem-gamal <tasneemgamal044@gmail.com>
2024-11-22 12:54:18 -0800

    add pay button

--- lib/features/checkout/ui/payment_datails_widgets/custom_credit_card.dart
+++ lib/features/checkout/ui/payment_datails_widgets/custom_credit_card.dart
@@ -41,7 +41,7 @@ class _CustomCreditCardState extends State<CustomCreditCard> {
             setState(() {});
           }, 
           formKey: formKey
-        )
+        ),
       ],
     );
   }
--- lib/features/checkout/ui/payment_details_view.dart
+++ lib/features/checkout/ui/payment_details_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:payment/core/widgets/constants.dart';
+import 'package:payment/core/widgets/custom_button.dart';
 import 'package:payment/features/checkout/ui/payment_datails_widgets/payment_details_view_body.dart';
 
 class PaymentDetailsView extends StatelessWidget {
@@ -6,8 +9,15 @@ class PaymentDetailsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(child: PaymentDetailsViewBody()),
+    return Scaffold(
+      body: const SafeArea(child: PaymentDetailsViewBody()),
+      bottomNavigationBar: Padding(
+        padding: Constants.checkoutPadding.copyWith(bottom: 12.h),
+        child: CustomButton(
+          onPressed: (){},
+          title: 'Pay'
+        ),
+      ),
     );
   }
 }
