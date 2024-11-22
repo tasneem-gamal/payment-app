5ecc0dbc7f935fa6f6bbea3d1d4bb682a5613676
tasneem-gamal <tasneemgamal044@gmail.com>
2024-11-22 12:12:06 -0800

    show credit card

---
+++ lib/features/checkout/ui/payment_datails_widgets/custom_credit_card.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/flutter_credit_card.dart';
+
+class CustomCreditCard extends StatefulWidget {
+  const CustomCreditCard({super.key});
+
+  @override
+  State<CustomCreditCard> createState() => _CustomCreditCardState();
+}
+
+class _CustomCreditCardState extends State<CustomCreditCard> {
+  String cardNumber= '', expiryDate = '', cardHolderName = '', cvvCode = '';
+
+  bool showBackView = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CreditCardWidget(
+          cardNumber: cardNumber, 
+          expiryDate: expiryDate, 
+          cardHolderName: cardHolderName, 
+          cvvCode: cvvCode, 
+          showBackView: showBackView, 
+          onCreditCardWidgetChange: (value){},
+          padding: 0,
+        )
+      ],
+    );
+  }
+}
