2edd1faab62a5f8583bc002a0db232254a75ece9
tasneem-gamal <tasneemgamal044@gmail.com>
2024-11-22 18:15:18 -0800

    refactor

--- lib/features/checkout/ui/payment_datails_widgets/payment_details_view_body.dart
+++ lib/features/checkout/ui/payment_datails_widgets/payment_details_view_body.dart
@@ -21,44 +21,52 @@ class _PaymentDetailsViewBodyState extends State<PaymentDetailsViewBody> {
   Widget build(BuildContext context) {
     return Padding(
       padding: Constants.checkoutPadding,
-      child: CustomScrollView(
-        slivers:[
-            const SliverToBoxAdapter(child: CustomAppBar(text: 'Payment Details')),
-            SliverToBoxAdapter(child: SizedBox(height: 24.h,)),
-            const SliverToBoxAdapter(child: PaymentMethodsListView()),
-            SliverToBoxAdapter(child: SizedBox(height: 16.h,)),
-            SliverToBoxAdapter(child: CustomCreditCard(
-              formKey: formKey,
-              autovalidateMode: autovalidateMode,
-            )),
-            SliverToBoxAdapter(child: SizedBox(height: 50.h,)),
-            SliverFillRemaining(
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: Padding(
-                  padding: EdgeInsets.only(bottom: 12.h),
-                  child: CustomButton(
-                    title: 'Pay',
-                    onPressed: (){
-                      if(formKey.currentState!.validate()){
-                        formKey.currentState!.save();
-                      } else {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (context) => const SuccessView()
-                          )
-                        );
-                        autovalidateMode = AutovalidateMode.always;
-                        setState(() {});
-                      }
-                    },
-                  ),
-                ),
+      child: CustomScrollView(slivers: [
+        const SliverToBoxAdapter(child: CustomAppBar(text: 'Payment Details')),
+        SliverToBoxAdapter(
+            child: SizedBox(
+          height: 24.h,
+        )),
+        const SliverToBoxAdapter(child: PaymentMethodsListView()),
+        SliverToBoxAdapter(
+            child: SizedBox(
+          height: 16.h,
+        )),
+        SliverToBoxAdapter(
+            child: CustomCreditCard(
+          formKey: formKey,
+          autovalidateMode: autovalidateMode,
+        )),
+        SliverToBoxAdapter(
+            child: SizedBox(
+          height: 50.h,
+        )),
+        SliverFillRemaining(
+          child: Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: EdgeInsets.only(bottom: 12.h),
+              child: CustomButton(
+                title: 'Pay',
+                onPressed: () {
+                  formValidate(context);
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const SuccessView()));
+                },
               ),
-            )
-        ]
-      ),
+            ),
+          ),
+        )
+      ]),
     );
   }
-}
 
+  void formValidate(BuildContext context) {
+    if (formKey.currentState!.validate()) {
+      formKey.currentState!.save();
+    } else {
+      autovalidateMode = AutovalidateMode.always;
+      setState(() {});
+    }
+  }
+}
