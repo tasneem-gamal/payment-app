61c4cab5a1e70824adb19e7dea06bed21f47c4f2
tasneem-gamal <tasneemgamal044@gmail.com>
2024-11-22 19:07:52 -0800

    success container

--- lib/features/checkout/ui/success_view_widgets.dart/success_view_body.dart
+++ lib/features/checkout/ui/success_view_widgets.dart/success_view_body.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:payment/core/widgets/constants.dart';
 
 class SuccessViewBody extends StatelessWidget {
   const SuccessViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: Constants.checkoutPadding.copyWith(bottom: 24),
+      child: Stack(
+        children: [
+          Container(
+            decoration: ShapeDecoration(
+              color: const Color(0xFFD9D9D9),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20)
+              )
+            ),
+          ),
+          Positioned(
+            left: -20,
+            bottom: MediaQuery.sizeOf(context).height * 0.2,
+            child: const CircleAvatar(
+              backgroundColor: Colors.white,
+            )
+          ),
+          Positioned(
+            right: -20,
+            bottom: MediaQuery.sizeOf(context).height * 0.2,
+            child: const CircleAvatar(
+              backgroundColor: Colors.white,
+            )
+          )
+        ],
+      ),
+    );
   }
 }
