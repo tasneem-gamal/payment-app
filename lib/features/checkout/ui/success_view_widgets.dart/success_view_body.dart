12b4fdb0717073e65553b32fd7d35b43d8a04de4
tasneem-gamal <tasneemgamal044@gmail.com>
2024-11-22 19:14:46 -0800

    check shape

--- lib/features/checkout/ui/success_view_widgets.dart/success_view_body.dart
+++ lib/features/checkout/ui/success_view_widgets.dart/success_view_body.dart
@@ -9,6 +9,7 @@ class SuccessViewBody extends StatelessWidget {
     return Padding(
       padding: Constants.checkoutPadding.copyWith(bottom: 24),
       child: Stack(
+        clipBehavior: Clip.none,
         children: [
           Container(
             decoration: ShapeDecoration(
@@ -30,6 +31,20 @@ class SuccessViewBody extends StatelessWidget {
             bottom: MediaQuery.sizeOf(context).height * 0.2,
             child: const CircleAvatar(
               backgroundColor: Colors.white,
+            )
+          ),
+          const Positioned(
+            top: -50,
+            left: 0,
+            right: 0,
+            child: CircleAvatar(
+              radius: 50,
+              backgroundColor: Color(0xFFD9D9D9),
+              child: CircleAvatar(
+                radius: 38,
+                backgroundColor: Color(0xff34A853),
+                child: Icon(Icons.check, size: 40, color: Colors.white,),
+              ),
             )
           )
         ],
