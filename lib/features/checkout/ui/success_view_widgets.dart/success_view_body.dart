e7e3d2e74a6953b7bba83e1bed1112b39625dc50
tasneem-gamal <tasneemgamal044@gmail.com>
2024-11-22 20:08:45 -0800

    refactor success view body

--- lib/features/checkout/ui/success_view_widgets.dart/success_view_body.dart
+++ lib/features/checkout/ui/success_view_widgets.dart/success_view_body.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:payment/core/widgets/constants.dart';
+import 'package:payment/features/checkout/ui/success_view_widgets.dart/custom_check_item.dart';
+import 'package:payment/features/checkout/ui/success_view_widgets.dart/custom_dashed_line.dart';
+import 'package:payment/features/checkout/ui/success_view_widgets.dart/success_card.dart';
 
 class SuccessViewBody extends StatelessWidget {
   const SuccessViewBody({super.key});
@@ -11,14 +14,7 @@ class SuccessViewBody extends StatelessWidget {
       child: Stack(
         clipBehavior: Clip.none,
         children: [
-          Container(
-            decoration: ShapeDecoration(
-              color: const Color(0xFFD9D9D9),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20)
-              )
-            ),
-          ),
+          const SuccessCard(),
           Positioned(
             left: -20,
             bottom: MediaQuery.sizeOf(context).height * 0.2,
@@ -37,38 +33,20 @@ class SuccessViewBody extends StatelessWidget {
             bottom: MediaQuery.sizeOf(context).height * 0.2 + 20,
             right: 28,
             left: 28,
-            child: Row(
-              children:
-                List.generate(
-                  30, 
-                  (index) => Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 2),
-                      child: Container(
-                        height: 2,
-                        color: const Color(0xffB8B8B8),
-                      ),
-                    )
-                  )
-                )
-            )
+            child: const CustomDashedLine()
           ),
           const Positioned(
             top: -50,
             left: 0,
             right: 0,
-            child: CircleAvatar(
-              radius: 50,
-              backgroundColor: Color(0xFFD9D9D9),
-              child: CircleAvatar(
-                radius: 38,
-                backgroundColor: Color(0xff34A853),
-                child: Icon(Icons.check, size: 40, color: Colors.white,),
-              ),
-            )
+            child: CustomCheckItem()
           )
         ],
       ),
     );
   }
 }
+
+
+
+
